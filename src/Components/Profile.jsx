725eea0a4ccc191d62d685da1e9b8f4224fd4bfe
krishnan681 <gopalakrishnan0614@gmail.com>
2025-02-24 15:10:19 +0530

    Total count

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -26,7 +26,7 @@ const QRCodeGenerator = () => {
             onClick={() => setSelectedPlan(plan)}
             className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
           >
-             {plan.name} - ₹{plan.amount}
+            Buy {plan.name} - ₹{plan.amount}
           </button>
         ))}
       </div>
